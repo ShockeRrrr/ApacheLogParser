ae4810ab0587035d4a637430dee718bf64d9d805
ShockeRrrr <kiler3969@gmail.com>
2021-04-11 07:50:11 +0300

    try to create a parser

--- LogParser/models.py
+++ LogParser/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class LogEntry(models.Model):
+    id = models.AutoField(primary_key=True)
+    ip = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.id
--- LogParser/views.py
+++ LogParser/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    if request.method == 'POST':
+        f = request.FILES['file'].read()
+        txt = str(f.decode('utf-8'))
+        pattern = r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}'
+        ips = re.findall(pattern, txt)
+        return render(request, 'index.html', ips)
+    return render(request, 'index.html')
