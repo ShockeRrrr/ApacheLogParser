6af6798d299180d703cf4f122a7a551417f90e26
ShockeRrrr <kiler3969@gmail.com>
2021-04-14 05:21:54 +0300

    fixed bugs

--- LogParser/management/commands/parser.py
+++ LogParser/management/commands/parser.py
@@ -6,14 +6,15 @@ from django.core.management.base import BaseCommand, CommandError
 from tqdm import tqdm
 
 from LogParser.models import LogEntry
-    class Command(BaseCommand):
+    
+class Command(BaseCommand):
         help = 'command for parsing a log and save it to data ( get a url for argument)'
     # Комплируем в переменную LOG_REGEX регулярки для парса строки по типу
     # После такой компиляции можно использовать переменную для обнаружения в строке по всем регуляркам сразу
     # 13.66.139.0 - - [19/Dec/2020:13:57:26 +0100] "GET /index HTTP/1.1" 200 32653 "-" "Mozilla/5.0" "-"
         LOG_REGEX = re.compile(
             os.getenv('LOG_REGEX', r'(?P<ip>.+) (?P<user_id>.+) (?P<user_name>.+) \[(?P<date>.+)\] "(?P<method>.+) '
-                               r'\(?P<request_path>.+) HTTP\/(?P<http_version>.+)" (?P<status_code>\d+) '
+                               r'(?P<request_path>.+) HTTP\/(?P<http_version>.+)" (?P<status_code>\d+) '
                                r'(?P<response_size>.*) "(?P<referrer>.*)" "(?P<user_agent>.*)" "-"'))
         # В качестве аргумента указываем ссылку на лог
         # python manage.py parser http://www.almhuette-raith.at/apache-log/access.log
@@ -36,7 +37,7 @@ from LogParser.models import LogEntry
 
             # Это делается для того, чтобы предотвратить загрузку всего ответа в память сразу
             # если размер ответа равен 1000, а chunk_size установлен на 100, мы разделим ответ на десять частей.
-                for chunk in r.iter_content(chunk_size=8192):
+                for chunk in response.iter_content(chunk_size=8192):
                     if chunk: # bool = True
                         for line in chunk.decode('utf-8').splitlines(): # Преобразуем байты в строку
                             #и разобьем на множество строк, возвращая их списком
